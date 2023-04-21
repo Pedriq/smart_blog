7da2dc1c964fa41d3ff400a3c5ae4f704a5e4ff8
Pedriq <1pedre.off@gmail.com>
2023-04-21 22:23:06 +0300

    add new apps

--- src/congig/settings.py
+++ src/congig/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'blogs',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Users
+AUTH_USER_MODEL = 'users.User'
