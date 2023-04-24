3781134bb87042c96a2390c27684a38ed0cd5a6c
Pedriq <1pedre.off@gmail.com>
2023-04-24 23:13:17 +0300

    sorting and registration of models

--- src/blogs/models.py
+++ src/blogs/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+
 from users.models import User
+
 
 class Blog(models.Model):
     title = models.CharField(max_length=128)
     description = models.TextField(null=True, blank=True)
     date_of_creation = models.DateTimeField(auto_now_add=True)
+
 
 class Article(models.Model):
     title = models.CharField(max_length=128)
@@ -21,7 +24,7 @@ class Rating(models.Model):
         (4, 'Хорошо'),
         (5, 'Отлично'),
     ]
-    
+
     article = models.ForeignKey(to=Blog, on_delete=models.CASCADE, null=False)
     user = models.ForeignKey(to=User, on_delete=models.CASCADE, null=False)
     rating = models.IntegerField(default=0, choices=ARTICLE_CHOICES)
