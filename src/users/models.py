37e19bd235a2361e9768511fbc03033b47e8cc08
Pedriq <1pedre.off@gmail.com>
2023-04-25 17:04:36 +0300

    Revert "Revert " New architecture""
    This reverts commit 2405e37670e511be35e7962e252b5ed93febce62.

--- src/users/models.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-
-class User(AbstractUser):
-    image = models.ImageField(upload_to='users_image', null=True, blank=True)
